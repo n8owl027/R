965d5cb2cd112b7462951cfbd4cfccf9510bd0fb
98owl <111882967+n8owl027@users.noreply.github.com>
2022-09-06 01:46:18 +0600

    Add files via upload

---
+++ combindtwovect.r
@@ -0,0 +1,8 @@
+# here x and y are two vectors
+x <- c(1, 2, 3, 4, 5, 6)
+y <- c(20, 30, 40, 50, 60,70)
+#both have same number of elements
+# x has 6 elements also
+# y has 6 elements
+#cbind(x , y )
+cbind(x, y)
